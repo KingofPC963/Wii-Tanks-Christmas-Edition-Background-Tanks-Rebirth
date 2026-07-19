87784307aef0833fbcd2765a691abb64ff4c4a03
KingofPC963 <139511952+KingofPC963@users.noreply.github.com>
2026-07-19 18:04:00 -0400

    Add files via upload

---
+++ Christmas 1 Background Mod/Christmas 1 Background Mod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TanksRebirth.GameContent.ModSupport;
+using TanksRebirth.Localization;
+
+namespace Christmas1BackgroundMod;
+
+public class Christmas1BackgroundMod : TanksMod
+{
+    public override void OnLoad(){
+    using ImportAsset<T>("C:\Users\joeto\Documents\My Games\Tanks Rebirth\Mods\Christmas 1 Background Mod\Tanks\outer_bounds")
+    }
+    public override void OnUnload(){
+    "=null"
+    }
+}
